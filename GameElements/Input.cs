d1807825b6d9343a602aacf1d7298601e65c35bc
Tourmi <domino_b10@hotmail.com>
2021-03-30 14:23:49 -0400

    #10 - Added default AI

---
+++ GameElements/Input.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongML.GameElements
+{
+    public enum Direction
+    {
+        None,
+        Up,
+        Down
+    }
+
+    public class Input
+    {
+        public Direction Direction { get; set; }
+        private double intensity;
+        public double Intensity
+        {
+            get => intensity; set
+            {
+                intensity = value;
+                if (intensity > 1)
+                {
+                    intensity = 1;
+                }
+                if (intensity < 0)
+                {
+                    intensity = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongML.Models
+{
+    class Game
+    {
+        public int CurrentFrame { get; set; }
+    }
+}
