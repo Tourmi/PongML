8ca4eb77a357161b804694d7de4333ad9dcda036
Jeremy Potvin <Potvin98@LIVE.CA>
2021-04-02 16:56:35 -0400

    Can save and load configuration settings

--- Models/GameConfiguration.cs
+++ Models/GameConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -71,5 +72,8 @@ namespace PongML.Models
             this.BallSpeedIncrement = ballSpeedIncrement;
             this.PaddleSpeed = paddleSpeed;
         }
+        public static GameConfiguration FromJson(string json) => JsonConvert.DeserializeObject<GameConfiguration>(json);
+
+        public string ToJson() => JsonConvert.SerializeObject(this, Formatting.Indented);
     }
 }
