c16d62bd678f2c11a48f6c53baaef4e3bbef9e4e
Jeremy Potvin <Potvin98@LIVE.CA>
2021-04-06 17:48:57 -0400

    #24 - Start, start from file and stop training working.

--- Training.xaml.cs
+++ Training.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using PongML.SimulationElements;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,26 +25,104 @@ namespace PongML
     public partial class Training : Page
     {
         private Models.GameConfiguration gc;
+        private Simulation sim;
         public Training(Models.GameConfiguration gc)
         {
             InitializeComponent();
             this.gc = gc;
-        }
-
-        private void Btn_TrainingOptions_Click(object sender, RoutedEventArgs e)
-        {
-
+            Btn_StopTraining.IsEnabled = false;
         }
 
         private void Btn_Training_Click(object sender, RoutedEventArgs e)
         {
+            if (sim != null && !sim.Ready)
+            {
+                return;
+            }
 
+            sim = new Simulation(gc);
+
+            sim.NewGeneration += OnNewRound;
+
+            ThreadPool.QueueUserWorkItem(p => sim.Start());
+
+            Btn_StopTraining.IsEnabled = true;
+            Btn_StartTraining.IsEnabled = false;
+            Btn_StartTrainingFromFile.IsEnabled = false;
+        }
+
+        private void Btn_StopTraining_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                sim.Stop();
+                sim.NewGeneration -= OnNewRound;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failure when stopping the simulation", ex);
+            }
+
+            Btn_StopTraining.IsEnabled = false;
+            Btn_StartTraining.IsEnabled = true;
+            Btn_StartTrainingFromFile.IsEnabled = true;
+        }
+
+        private void Btn_TrainingFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (sim != null && !sim.Ready)
+            {
+                return;
+            }
+
+            GameElements.AI.Brain brain = null;
+
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Json file (*.json)|*.json";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                brain = GameElements.AI.Brain.FromJson(File.ReadAllText(openFileDialog.FileName));
+            }
+
+            if (brain == null)
+            {
+                MessageBox.Show("Failed to load the AI file.");
+                return;
+            }
+
+            sim = new Simulation(brain, gc);
+
+            sim.NewGeneration += OnNewRound;
+
+            ThreadPool.QueueUserWorkItem(p => sim.Start());
+
+            Btn_StopTraining.IsEnabled = true;
+            Btn_StartTraining.IsEnabled = false;
+            Btn_StartTrainingFromFile.IsEnabled = false;
         }
 
         private void Btn_BackMainMenu_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                sim.Stop();
+                sim.NewGeneration -= OnNewRound;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failure when stopping the simulation", ex);
+            }
+
             var window = Window.GetWindow(this);
             window.Content = new MainMenu(gc);
+        }
+
+        private void OnNewRound()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                TextBlock_Round.Text = $"Generation: {sim.Round}";
+            }, System.Windows.Threading.DispatcherPriority.Render);
         }
     }
 }
